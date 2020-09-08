01b69fe73980d1adff7268d8712413e56f0b37f9
Mark Slater <markaaronslater@gmail.com>
2020-09-08 11:16:08 -0400

    delete irrelevant statistics from get_stats

--- src/investigative/get_stats.py
+++ src/investigative/get_stats.py
@@ -13,19 +13,10 @@ from pickle import load, dump, HIGHEST_PROTOCOL
 
 
 
-# expects src_sentences and trg_sentences to be lists of str(sentence)'s
-def get_src_trg_pairs(src_sentences, trg_sentences, start=1, num=1):
-    # use 1-based indexing so that matches UNIX line numbering of files
-    pairs = []
-    for i in range(start-1, start+num-1):
-        pairs.append((src_sentences[i], "#####", trg_sentences[i]))
-    return pairs
-
-
 
 # compute histogram of sentence lengths of src and trg corpuses, in order to determine appropriate universal pad size,
 # mask ranges, etc.
-def histoLengths(src_sentences, trg_sentences):
+def get_lengths_histogram(src_sentences, trg_sentences):
     src_lengthCounts = Counter()
     for src_sentence in src_sentences:
         src_lengthCounts.update([len(src_sentence.split())]) #num tokens, not chs...
@@ -44,7 +35,7 @@ def histoLengths(src_sentences, trg_sentences):
         print(item)
 
 
-def getStats(src_sentences, trg_sentences):
+def compute_length_stats(src_sentences, trg_sentences):
     src_lengths = []
     trg_lengths = []
     diffs = []
@@ -61,74 +52,7 @@ def getStats(src_sentences, trg_sentences):
 
 
 
-
-# produce corpus counterparts with line-numbering, to ease corresponding prediction look-ups, etc.
-def numberCorpuses(path, train_src_sentences, train_trg_sentences, dev_src_sentences, dev_trg_sentences, test_src_sentences, filtered=True):
-    prefix = "num_"
-    if filtered:
-        train_prefix = "numf_" # only makes a difference for the training sets
-    else:
-        train_prefix = prefix
-    with open(path + train_prefix + "train.de", "w") as f:
-        # use 1-based indexing
-        for i, sent in enumerate(train_src_sentences):
-            f.write(str(i+1) + ': ' + sent + '\n')
-   
-    with open(path + train_prefix + "train.en", "w") as f:
-        for i, sent in enumerate(train_trg_sentences):
-            f.write(str(i+1) + ': ' + sent + '\n')
-   
-    with open(path + prefix + "dev.de", "w") as f:
-        for i, sent in enumerate(dev_src_sentences):
-            f.write(str(i+1) + ': ' + sent + '\n')
-   
-    with open(path + prefix + "dev.en", "w") as f:
-        for i, sent in enumerate(dev_trg_sentences):
-            f.write(str(i+1) + ': ' + sent + '\n')
-   
-    with open(path + prefix + "test.de", "w") as f:
-        for i, sent in enumerate(test_src_sentences):
-            f.write(str(i+1) + ': ' + sent + '\n')
-
-
-# acronyms containing at least one period
-def getRegex(train_sentences):
-    #acronyms = re.compile(r'\w+\.\w+[.\w]*')
-    #acronyms = re.compile(r'\b\w\w\.\s') 
-    acronyms = re.compile(r'\W\'\w+|\w+\'\W') 
-
-
-    for sent in train_sentences:
-        acs = acronyms.findall(sent)
-        if len(acs) > 0:
-            print(acs)
-
-
-
-def geteosinfo(sentences):
-    eosSymbols = ['.','?','!', '"', '♫', ']', ':', '♪', ',']
-    numSent = 0 # num sentences not ending in .
-    for sent in sentences:
-        if sent[-1] not in eosSymbols:
-            print(sent)
-            numSent += 1
-    print(numSent)
-
-def getspecialCharinfo(sentences):
-    numSent = 0 # num sentences not ending in .
-    
-    for i, sent in enumerate(sentences):
-        new_sent = normalize('NFD', sent).encode('ascii', 'ignore')
-        new_sent = new_sent.decode('UTF-8')
-        if sent != new_sent:
-            print("{} {}".format(i, sent))
-            print(new_sent)
-            print()
-            numSent += 1
-    print(numSent)
-
-
-def getsongs(sentences):
+def get_songs(sentences):
     numSent = 0 # num sentences not ending in .
     
     for i, sent in enumerate(sentences):
@@ -137,120 +61,3 @@ def getsongs(sentences):
             print()
             numSent += 1
     print(numSent)
-
-
-def multiplePeriods(sentences):
-    numSent = 0 
-    
-    for i, sent in enumerate(sentences):
-        #sent = ' '.join(sent)
-        if sent.count('.') > 1:
-            
-            print("{} {}".format(i, sent))
-            print()
-            numSent += 1
-    print(numSent)
-
-
-
-def multipleQuotes(sentences):
-    numSent = 0 
-    
-    for i, sent in enumerate(sentences):
-        if sent.count('"') > 0:
-            
-            print("{} {}".format(i, sent))
-            print()
-            numSent += 1
-    print(numSent)
-
-
-
-
-
-# must pass corpuses in expected order
-def naiveCorpusSegmenter(corpuses):
-    acronyms = re.compile(r'\w+\.\w+[.\w]*|Mr\.|Mrs\.|Ms\.|Dr\.|St\.|Mt\.|Lt\.|lbs?\.|sq\.|ft\.') 
-    ellipses = re.compile(r'(\.\.\.*)')
-    corpus_names = ["train.de", "train.en", "dev.de", "dev.en", "test.de"]
-    for corpus_idx, corpus in enumerate(corpuses):
-        corpus_name = corpus_names[corpus_idx]
-        with open(path + "seg_" + corpus_name, "w") as f:
-            print("segmenting " + corpus_name + "...")
-            #num = 0
-            for sent in corpus:
-                # overwrite sentence such that each ellipse surrounded by whitespace
-                s = re.sub(ellipses, r' \1 ', sent)
-
-                # get locations of new string spanned by the ellipses
-                ellipseSpans = []
-                for m in ellipses.finditer(s):
-                    ellipseSpans += list(range(m.start(), m.end())) # flatten list
-                    
-                # get locations of the new string spanned by the acronyms
-                acronymSpans = []
-                for m in acronyms.finditer(s):
-                    acronymSpans += list(range(m.start(), m.end())) 
-
-                # remove them from consideration when segmenting periods of sentence
-                possiblePeriods = set(range(len(s))) - set(acronymSpans) - set(ellipseSpans)
-                realPeriods = [i for i in possiblePeriods if s[i] == '.']
-
-                if len(realPeriods) > 0:
-                    end = realPeriods[0]
-                    segmented_sent = s[:end]
-                    start = end
-                    for end in realPeriods[1:]:
-                        # add space to the left of each real period: ex) hey. -> hey .
-                        segmented_sent = segmented_sent + ' ' + s[start:end]
-                        start = end
-                    segmented_sent = segmented_sent + ' ' + s[end]
-                else:
-                    segmented_sent = s
-
-                # if corpus_name == "train.en":
-                #     if len(realPeriods) > 1:
-                #         print(segmented_sent)
-                #         num += 1
-                f.write(segmented_sent + '\n')
-            #print("{} lines contain more than one sentence".format(num))
-            #print()
-
-
-
-
-
-if __name__=='__main__':
-    # run from within code directory
-    path = "../iwslt16_en_de/" # path to corpuses
-    # texts = load_docs(path)
-    # texts = to_sentences(texts)    
-    # train_src_sentences, train_trg_sentences, dev_src_sentences, dev_trg_sentences, test_src_sentences = texts[0], texts[1], texts[2], texts[3], texts[4]
-    # train_src_sentences, train_trg_sentences = texts[0], texts[1]
-    # train_src_sentences, train_trg_sentences = filterSentences(train_src_sentences, train_trg_sentences)
-    # texts[0], texts[1] = train_src_sentences, train_trg_sentences
-
-    # normalizeCorpuses(texts, path, decase=False) #do not decase when construct namesTable
-
-  
-    #numberCorpuses(path, train_src_sentences, train_trg_sentences, dev_src_sentences, dev_trg_sentences, test_src_sentences, filtered=True)
-    #multiplePeriods(dev_trg_sentences)
-    #multipleQuotes(dev_trg_sentences)
-
-    norm_ref_texts = load_docs(path, ref=True)
-    norm_ref_texts = to_sentences(norm_ref_texts)
-    train_src_sentences, train_trg_sentences, dev_src_sentences, dev_trg_sentences, test_src_sentences = norm_ref_texts[0], norm_ref_texts[1], norm_ref_texts[2], norm_ref_texts[3], norm_ref_texts[4]
-
-
-    de_namesTable, de_count = createNamesTable(path, "names.de", train_src_sentences)
-    # store the dict as in a pickle file
-    # with open(path + "names.de", 'wb') as f:
-    #     dump(de_namesTable, f)
-
-    en_namesTable, en_count = createNamesTable(path, "names.en", train_trg_sentences)
-    # with open(path + "names.en", 'wb') as f:
-    #     dump(en_namesTable, f)
-    print("de names count: {}".format(de_count))
-    print("en names count: {}".format(en_count))
-
-    # still need to use Iwords, bc i and i'm occurred a few times in the trainset, etc.
